b78b949cb7237408391217d3a0f36203b8439f5b
edgar <edgar_harutyunyan_2025@mail.ru>
2024-11-18 13:50:55 +0400

    new

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-#echo "Hello $1 $2"
-#time=$(date)
-#echo "time=$time" >> $GITHUB_OUTPUT
-docker run  --rm -p $1 -d action
+echo "Hello $1 $2"
+time=$(date)
+echo "time=$time" >> $GITHUB_OUTPUT
+#docker run  --rm -p $1 -d action
