5061da96bf8c69d6e33ba65343d67ba14a7f6e1f
edgar <edgar_harutyunyan_2025@mail.ru>
2024-11-18 15:14:48 +0400

    sum num

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -l
 
 echo "num1= $1"
-echo "num2 =$2"
-echo "sum = ${{$num1 + $num2}}" >> $GITHUB_OUTPUT
+echo "num2= $2"
+echo "sum = $(($num1 + $num2))" >> $GITHUB_OUTPUT
+
 #time=$(date)
 #echo "time=$time" >> $GITHUB_OUTPUT
 #docker run  --rm -p $1 -d action
